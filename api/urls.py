9df0c684b4c223c69c2aa00fac84ee9af1bfa69e
Nihal-Abedin <nihal.abedin.annon99@gmail.com>
2024-01-09 03:22:17 +0600

    reset pass added

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('user/token/', userAuths_views.MyTokenObtainPairView.as_view()),
     path('user/token/refresh', TokenRefreshView.as_view()),
-    path('user/register/', userAuths_views.RegisterView.as_view())
+    path('user/register/', userAuths_views.RegisterView.as_view()),
+    path('user/reset-password/<email>', userAuths_views.ResetUserPasswordEmail.as_view()),
+    path('user/set-password/', userAuths_views.PasswordChangeView.as_view())
 ]
--- user_auths/views.py
+++ user_auths/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from user_auths.models import User, Profile
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from user_auths.serializer import MyTokenObtainPairSerializer, RegisterSerializer
+from user_auths.serializer import MyTokenObtainPairSerializer, RegisterSerializer, UserSerializer
+import shortuuid
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -13,7 +16,57 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
-    permission_classes = (AllowAny, )
+    permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
 
 
+def generate_otp():
+    uuid_key = shortuuid.uuid()
+    unique_key = uuid_key[:7]
+
+    return unique_key
+
+
+class ResetUserPasswordEmail(generics.RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        email = self.kwargs['email']
+        user = User.objects.get(email=email)
+
+        print(user)
+
+        if user:
+            user.otp = generate_otp()
+            user.save()
+
+            user_otp = user.otp
+            user_id = user.pk
+            link = f'http://127.0.0.1:30001/create-password?otp={user_otp}&id={user_id}'
+            print(link)
+            # SEND EMAIL
+        return user
+
+
+class PasswordChangeView(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        payload = request.data
+
+        otp = payload['otp']
+        password = payload['password']
+        uid = payload['uuid']
+
+        user = User.objects.get(otp=otp, id=uid)
+
+        if user:
+            user.set_password(password)
+            user.otp = ''
+            user.save()
+
+            return Response({"message": "Password changed successfully!"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "User doesn't exists"}, status=status.HTTP_404_NOT_FOUND)
